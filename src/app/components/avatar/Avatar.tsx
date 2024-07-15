47d7c5224d3ba3499fd02f70295b663ab8427f32
Galartza <jotaquin99g@gmail.com>
2024-07-15 16:31:34 -0300

    Features: Folder img with SVG file was added; updated allowed domain configurations in next.config.mjs

--- src/app/components/avatar/Avatar.tsx
+++ src/app/components/avatar/Avatar.tsx
@@ -1,49 +1,56 @@
-// Asegura que este componente se ejecute en el cliente
-'use client'; 
-
-// Importa React, necesario para crear componentes de React
-import React from 'react';
+// Importa React y los hooks necesarios
+import React, { useState, useEffect } from 'react';
 
 // Importa el archivo CSS específico para estilizar este componente
 import "./avatar-modules.css";
 
+// Importa el componente Image de Next.js
+import Image from 'next/image';
+
+// Importa la imagen SVG predeterminada
+import defaultImageUrl from '../../../img/avatarDefaultIMG.svg';
+
 // Define los tipos de las props que el componente Avatar espera recibir
 interface AvatarProps {
-  // Propiedad opcional para la URL de la imagen del avatar, puede ser una cadena o estar ausente
-  imageUrl?: string;
-  // Propiedad requerida para el nivel del usuario, debe ser un número
-  level: number;
+  imageUrl?: string; // Propiedad opcional para la URL de la imagen del avatar
+  level: number; // Propiedad requerida para el nivel del usuario
 }
 
 // Define el componente Avatar como un componente funcional de React
-// El componente acepta props que cumplen con la interfaz AvatarProps
 const Avatar: React.FC<AvatarProps> = ({ imageUrl, level }) => {
-  // URL de imagen predeterminada a usar si imageUrl no se proporciona o falla al cargar
-  const defaultImageUrl = "https://via.placeholder.com/150"; 
+  // Estado para manejar la URL de la imagen actual
+  const [currentImageUrl, setCurrentImageUrl] = useState(imageUrl || defaultImageUrl);
+
+  // Efecto para actualizar la URL de la imagen cuando imageUrl cambie
+  useEffect(() => {
+    setCurrentImageUrl(imageUrl || defaultImageUrl);
+  }, [imageUrl]);
+
+  // Maneja el error de carga de la imagen
+  const handleImageError = () => {
+    setCurrentImageUrl(defaultImageUrl);
+  };
 
   // Retorna la estructura TSX del componente
   return (
-    // Contenedor principal del avatar, aplica estilos desde el CSS importado
     <div className='container-avatar'>
-        {/* Contenedor de la imagen del avatar */}
-        <div className='container-img'>
-            {/* Imagen del avatar; usa imageUrl si se proporciona, de lo contrario usa defaultImageUrl */}
-            {/* onError cambia la imagen a defaultImageUrl si hay un error al cargar imageUrl */}
-            <img 
-              className='img-avatar' 
-              src={imageUrl || defaultImageUrl} 
-              alt="Avatar" 
-              onError={(e) => { e.currentTarget.onerror = null; e.currentTarget.src = defaultImageUrl; }} 
-            />
-        </div>
-        {/* Contenedor del nivel del usuario */}
-        <div className='container-nivel'>
-            {/* Muestra el nivel del usuario pasado como prop */}
-            <h1 className='level'>Nivel {level}</h1>
-        </div>
+      <div className='container-img'>
+        <Image 
+          className='img-avatar' 
+          src={currentImageUrl} 
+          alt="Avatar" 
+          width={150}
+          height={150}
+          onError={handleImageError}
+        />
+      </div>
+      <div className='container-nivel'>
+        <h1 className='level'>Nivel {level}</h1>
+      </div>
     </div>
   );
 }
 
 // Exporta el componente Avatar para que pueda ser utilizado en otros archivos
 export default Avatar;
+
