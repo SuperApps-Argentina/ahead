8841b6bf2b604c9d5177ef79eb7d35838e6ad9db
Goyo Regalado <goyoregalado@gmail.com>
2024-08-05 10:35:40 +0100

    chores: removing some comments and improving spacing

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,4 +1,5 @@
 'use client'; 
+
 import React from 'react';
 
 import styles from './Avatar.module.css';
@@ -14,7 +15,10 @@ const Avatar: React.FC<AvatarProps> = ({ imageUrl, level }) => {
 
   return (
     <div className={styles.containerAvatar}>
+        {/* Contenedor de la imagen del avatar */}
         <div className={styles.containerImg}>
+            {/* Imagen del avatar; usa imageUrl si se proporciona, de lo contrario usa defaultImageUrl */}
+            {/* onError cambia la imagen a defaultImageUrl si hay un error al cargar imageUrl */}
             <img 
               className={styles.imgAvatar} 
               src={imageUrl || defaultImageUrl} 
@@ -22,11 +26,14 @@ const Avatar: React.FC<AvatarProps> = ({ imageUrl, level }) => {
               onError={(e) => { e.currentTarget.onerror = null; e.currentTarget.src = defaultImageUrl; }} 
             />
         </div>
+        {/* Contenedor del nivel del usuario */}
         <div className={styles.containerLevel}>
+            {/* Muestra el nivel del usuario pasado como prop */}
             <h1 className={styles.level}>Nivel {level}</h1>
         </div>
     </div>
   );
 }
 
+// Exporta el componente Avatar para que pueda ser utilizado en otros archivos
 export default Avatar;
