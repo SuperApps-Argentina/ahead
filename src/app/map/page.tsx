b01c733584f03bb732b5a60b9f5db5188976461a
Galartza <jotaquin99g@gmail.com>
2024-09-30 22:45:38 -0300

    adding map screen

---
+++ src/app/map/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LocationTracker from '../../components/LocationTracker/LocationTracker'; // Asegúrate de ajustar la ruta según tu estructura de archivos
+
+const MapPage: React.FC = () => {
+  return (
+    <div>
+      <LocationTracker />
+    </div>
+  );
+};
+
+export default MapPage;
