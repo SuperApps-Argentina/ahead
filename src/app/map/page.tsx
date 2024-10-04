56831e1f6a150f0aaeaf56ee55fe5ce4fc815eca
Galartza <jotaquin99g@gmail.com>
2024-10-04 16:12:16 -0300

    adding configurations to component map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -2,31 +2,33 @@
 "use client"; // Asegúrate de que este componente sea un Client Component
 
 import React, { useState } from 'react';
-import LocationTracker from '../../components/LocationTracker/LocationTracker';
-import MapLocation from '../../components/MapLocation/MapLocation';
+// import LocationTracker from '../../components/LocationTracker/LocationTracker';
+// import MapLocation from '../../components/MapLocation/MapLocation';
+import Map from '../../components/Map/Map'
 import styles from './page.module.css'; // Asegúrate de que el archivo CSS esté en la carpeta correcta
 
 const MapPage: React.FC = () => {
-  const [position, setPosition] = useState<{ lat: number; lng: number } | null>(null);
-  const [path, setPath] = useState<{ lat: number; lng: number }[]>([]);
-  const [tracking, setTracking] = useState<boolean>(false);
+  // const [position, setPosition] = useState<{ lat: number; lng: number } | null>(null);
+  // const [path, setPath] = useState<{ lat: number; lng: number }[]>([]);
+  // const [tracking, setTracking] = useState<boolean>(false);
 
-  const handleLocationUpdate = (newPosition: { lat: number; lng: number }) => {
-    setPosition(newPosition);
-    setPath(prevPath => [...prevPath, newPosition]);
-  };
+  // const handleLocationUpdate = (newPosition: { lat: number; lng: number }) => {
+  //   setPosition(newPosition);
+  //   setPath(prevPath => [...prevPath, newPosition]);
+  // };
 
-  const toggleTracking = () => {
-    setTracking(prev => !prev);
-  };
+  // const toggleTracking = () => {
+  //   setTracking(prev => !prev);
+  // };
 
   return (
     <div className={styles.container}>
-      <LocationTracker onLocationUpdate={handleLocationUpdate} tracking={tracking} />
+      {/* <LocationTracker onLocationUpdate={handleLocationUpdate} tracking={tracking} />
       <MapLocation position={position} path={path} onToggleTracking={toggleTracking} isTracking={tracking} />
       <button onClick={toggleTracking} className={styles.debugButton}>
         {tracking ? 'Detener' : 'Iniciar'}
-      </button>
+      </button> */}
+      <Map/>
     </div>
   );
 };
